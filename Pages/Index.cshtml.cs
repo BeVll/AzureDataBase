4fac4bbcf847c363a80d0f4c11f9de19c9ab6ddf
Bevll <vladixerplay@gmail.com>
2023-03-26 10:47:46 +0300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+	public class IndexModel : PageModel
+	{
+        // Create a DataAccessController object
+        DataAccessController dac = new DataAccessController();
+
+        // Create a collection for holding CoursesAndModules object
+        public List<CoursesAndModules> CoursesAndModules;
+
+        public void OnGet()
+        {
+            // Retrieve the data using the DataAccessController object and populate the CoursesAndModules object
+            CoursesAndModules = dac.GetAllCoursesAndModules().ToList();
+        }
+    }
+}
